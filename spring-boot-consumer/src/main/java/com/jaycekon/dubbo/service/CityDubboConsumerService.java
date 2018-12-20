38444e2de85bb4c6254d2e8095b5b68f87a294a5
guangjingfeng <guangjingfeng@gmail.com>
2018-12-20 11:00:00 +0800

    dubbo user

--- spring-boot-consumer/src/main/java/com/jaycekon/dubbo/service/CityDubboConsumerService.java
+++ spring-boot-consumer/src/main/java/com/jaycekon/dubbo/service/CityDubboConsumerService.java
@@ -1,7 +1,6 @@
 package com.jaycekon.dubbo.service;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.jaycekon.dubbo.domain.City;
 import com.jaycekon.dubbo.domain.User;
 import org.springframework.stereotype.Component;
 
@@ -13,18 +12,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class CityDubboConsumerService {
 
-    @Reference
-    CityDubboService cityDubboService;
 
     @Reference
     UserService userService;
-
-    public void printCity() {
-        String cityName = "广州";
-        City city = cityDubboService.findCityByName(cityName);
-        System.out.println(city.toString());
-    }
-
 
     public User saveUser() {
         User user = new User();
