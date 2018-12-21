f94cf7c947338c1b6337b1643635c3aee0c7effe
guangjingfeng <guangjingfeng@gmail.com>
2018-12-21 18:17:25 +0800

    quartz

---
+++ spring-boot-api/src/main/java/com/open/springqianbailu/service/quartz/SchedulerService.java
@@ -0,0 +1,53 @@
+package com.open.springqianbailu.service.quartz;
+
+import org.quartz.SchedulerException;
+
+public interface SchedulerService {
+
+    /**
+     * 开始执行所有任务
+     *
+     * @throws SchedulerException
+     */
+    void startJob() throws SchedulerException;
+
+    /**
+     * 获取Job信息
+     *
+     * @param name
+     * @param group
+     * @return
+     * @throws SchedulerException
+     */
+    String getJobInfo(String name, String group) throws SchedulerException;
+
+    /**
+     * 修改某个任务的执行时间
+     *
+     * @param name
+     * @param group
+     * @param time
+     * @return
+     * @throws SchedulerException
+     */
+      boolean modifyJob(String name, String group, String time) throws SchedulerException;
+
+
+    /**
+     * 暂停所有任务
+     *
+     * @throws SchedulerException
+     */
+     void pauseAllJob() throws SchedulerException;
+
+
+    /**
+     * 暂停某个任务
+     *
+     * @param name
+     * @param group
+     * @throws SchedulerException
+     */
+     void pauseJob(String name, String group) throws SchedulerException;
+
+}
