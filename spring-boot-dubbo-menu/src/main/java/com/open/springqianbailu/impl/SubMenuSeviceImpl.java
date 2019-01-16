f2b5ff16b1a83391640f06e989d2922975600031
guangjing.feng <guangjing.feng@oriente.com>
2019-01-16 17:52:28 +0800

    qianbailu菜单服务拆分

---
+++ spring-boot-dubbo-menu/src/main/java/com/open/springqianbailu/impl/SubMenuSeviceImpl.java
@@ -0,0 +1,57 @@
+package com.open.springqianbailu.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.open.springqianbailu.RedisUtil;
+import com.open.springqianbailu.dao.SubMenuMapper;
+import com.open.springqianbailu.model.table.SubMenu;
+import com.open.springqianbailu.service.SubMenuSevice;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+import static com.open.springqianbailu.RedisUtil.REDIS_EXPIRE_TIME;
+
+
+@Component
+@Service(interfaceClass = SubMenuSevice.class) //dubbo的service，注入接口
+public class SubMenuSeviceImpl   implements SubMenuSevice {
+    @Resource
+    public RedisUtil redisUtil;
+
+    @Resource
+    private SubMenuMapper subMenuMapper;
+
+    @Override
+    public int insert(SubMenu subMenu) {
+        return subMenuMapper.insert(subMenu);
+    }
+
+    @Override
+    public int insertSelective(SubMenu subMenu) {
+        return subMenuMapper.insertSelective(subMenu);
+    }
+
+    @Override
+    public List<SubMenu> selectByMenuId(Integer menuId) {
+        List<SubMenu> list = (List<SubMenu>) redisUtil.get("SubMenuSevice"+"selectByMenuId"+menuId);
+        if (list==null || list.size()==0){
+            list = subMenuMapper.selectByMenuId(menuId);
+            redisUtil.set("SubMenuSevice"+"selectByMenuId"+menuId,list,REDIS_EXPIRE_TIME);
+        }
+        return list;
+    }
+
+    @Override
+    public void doTaskMenu(List<SubMenu> list) {
+        if (list!=null && list.size()>0){
+            subMenuMapper.dropTable();
+            subMenuMapper.createTable();
+            for (SubMenu menu : list) {
+                menu.setUpdateTime(System.currentTimeMillis()+"");
+//                menuMapper.insert(menu);
+            }
+            subMenuMapper.insertBatch(list);
+        }
+    }
+}
---
+++ spring-boot-dubbo-menu/src/main/java/com/open/springqianbailu/impl/jsoup/JsoupSubMenuServiceImpl.java
@@ -0,0 +1,48 @@
+package com.open.springqianbailu.impl.jsoup;
+
+import com.open.springqianbailu.dao.SubMenuMapper;
+import com.open.springqianbailu.documents.MenuDocumentDao;
+import com.open.springqianbailu.model.table.SubMenu;
+import com.open.springqianbailu.service.jsoup.JsoupSubMenuService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Component
+@com.alibaba.dubbo.config.annotation.Service(interfaceClass = JsoupSubMenuService.class) //dubbo的service，注入接口
+public class JsoupSubMenuServiceImpl implements JsoupSubMenuService {
+
+    @Resource
+    private SubMenuMapper subMenuMapper ;
+
+    @Override
+    public void updateSubMenu() {
+        List<SubMenu> list = MenuDocumentDao.parseSubMenus();
+        if (list!=null && list.size()>0){
+            subMenuMapper.dropTable();
+            subMenuMapper.createTable();
+            for (SubMenu menu : list) {
+                menu.setUpdateTime(System.currentTimeMillis()+"");
+//                menuMapper.insert(menu);
+            }
+            subMenuMapper.insertBatch(list);
+        }
+//        List<Menu> listDb = menuMapper.selectAll();
+//        if (listDb == null || listDb.size()==0) {
+//            for (Menu menu : list) {
+//                menu.setUpdateTime(System.currentTimeMillis()+"");
+//                menuMapper.insert(menu);
+//            }
+//        }else {
+//            if (list.size()==listDb.size()) {
+//                for (int i = 0; i < listDb.size(); i++) {
+//                    Menu menu = list.get(i);
+//                    menu.setId(listDb.get(i).getId());
+//                    menu.setUpdateTime(System.currentTimeMillis()+"");
+//                    menuMapper.updateById(menu);
+//                }
+//            }
+//        }
+    }
+}
